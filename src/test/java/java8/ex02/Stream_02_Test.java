67e4679a184f42c2251db835e979a0df6623082d
Manuel <tyranidas@hotmail.com>
2023-01-06 16:31:21 +0100

    tp2

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,7 +24,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des clients associés aux commandes
-		List<Customer> result = null;
+		List<Customer> result = orders.stream().map(o-> o.getCustomer()).toList();
 
 		assertThat(result, hasSize(8));
 	}
@@ -35,7 +35,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre de clients associés aux commandes
-		long result = 0;
+		long result = orders.stream().map(o-> o.getCustomer()).count();;
 
 		assertThat(result, is(8L));
 	}
@@ -46,7 +46,7 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des différents clients associés aux commandes (sans doublons)
-		List<Customer> result = null;
+		List<Customer> result = orders.stream().map(o-> o.getCustomer()).distinct().toList();
 
 		assertThat(result, hasSize(2));
 	}
@@ -57,7 +57,8 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre des différents clients associés aux commandes
-		long result = 0L;
+		long result = orders.stream().map(o-> o.getCustomer()).distinct().count();;
+
 
 		assertThat(result, is(2L));
 	}
@@ -71,7 +72,7 @@ public class Stream_02_Test {
 		 * TODO Calculer le chiffre d'affaires total de la pizzeria (somme des prix des
 		 * commandes)
 		 */
-		double result = 0.0;
+		double result = orders.stream().mapToDouble( o-> o.getPrice()).sum();
 
 		assertThat(result, is(10900.0));
 	}
@@ -84,7 +85,7 @@ public class Stream_02_Test {
 		/*
 		 * TODO Calculer le prix moyen d'une commande
 		 */
-		OptionalDouble result = null;
+		OptionalDouble result = orders.stream().mapToDouble( o-> o.getPrice()).average();
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.getAsDouble(), is(1362.5));
